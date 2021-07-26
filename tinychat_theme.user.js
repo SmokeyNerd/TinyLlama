dab8235b9628968805b6d898b24763208dbf6477
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2021-07-26 00:46:22 -0500

    Rounded Cam Corners

--- tinychat_theme.user.js
+++ tinychat_theme.user.js
@@ -3153,6 +3153,10 @@ background-color:transparent !important;
 :host, #videolist {
 height: 0px !important;
 }}
+
+.video.blacknight div.ratio-4-3 {
+	border-radius: 10px;
+}
 /*--------------------------------------------------MINIYT--------------------------------------------------------*/
 .llama-miniyt .videos-items:first-child:not(.hidden) {
     width: 100px !important;
